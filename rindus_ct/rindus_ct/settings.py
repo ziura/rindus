a7e2356a3a3f2a7b9784c76f67f141ed372b6ba4
alberto.ruiz-valdepenas <alberto.ruiz-valdepenas@external.engie.com>
2024-02-05 08:13:46 +0100

    Improve docker compose setup

--- rindus_ct/rindus_ct/settings.py
+++ rindus_ct/rindus_ct/settings.py
@@ -93,13 +93,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_api.authentication.BearerAuthentication'
